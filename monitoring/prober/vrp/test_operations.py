8dfeed22dcc8219e81c8048a6f27a9f7972b3e1a
Anastasia Lemetti <anlemett@gmail.com>
2022-04-11 13:27:22 +0200

    Testing: some operations tests back

--- monitoring/prober/vrp/test_operations.py
+++ monitoring/prober/vrp/test_operations.py
@@ -319,6 +319,313 @@ def test_create_op2_no_key(ids, vrp_session, vrp_session2):
   assert ids(OP1_TYPE) in missing_ops
 
 
+# Create Op2 successfully, referencing the pre-existing Subscription
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Subscription Sub2 created by scd_session2 user
+# Mutations: Operation Op2 created by vrp_session2 user
+def test_create_op2(ids, vrp_session, vrp_session2):
+  req = _make_op2_request()
+  req['subscription_id'] = ids(SUB2_TYPE)
+  req['key'] = [op1_ovn]
+  resp = vrp_session2.put('/operational_intent_references/{}'.format(ids(OP2_TYPE)), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+
+  data = resp.json()
+  op = data['operational_intent_reference']
+  assert op['id'] == ids(OP2_TYPE)
+  assert op['uss_base_url'] == URL_OP2
+  assert_datetimes_are_equal(op['time_start']['value'], req['vertiport_reservation']['time_start']['value'])
+  assert_datetimes_are_equal(op['time_end']['value'], req['vertiport_reservation']['time_end']['value'])
+  assert op['version'] == 1
+  assert 'subscription_id' in op
+  assert op['state'] == 'Accepted'
+  assert op.get('ovn', '')
+  
+  resp = vrp_session2.get('/operational_intent_references/{}'.format(ids(OP1_TYPE)), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  implicit_sub_id = resp.json()['operational_intent_reference']['subscription_id']
+
+  # USS2 should definitely be instructed to notify USS1's implicit Subscription of the new Operation
+  subscribers = _parse_subscribers(data.get('subscribers', []))
+  assert URL_SUB1 in subscribers, subscribers
+  assert implicit_sub_id in subscribers[URL_SUB1], subscribers[URL_SUB1]
+  
+  # USS2 should also be instructed to notify USS2's explicit Subscription of the new Operation
+  assert URL_SUB2 in subscribers, subscribers
+  assert ids(SUB2_TYPE) in subscribers[URL_SUB2], subscribers[URL_SUB2]
+  assert subscribers[URL_SUB2][ids(SUB2_TYPE)] == 1
+  
+  global op2_ovn
+  op2_ovn = op['ovn']
+
+
+# Op1 and Op2 should both be visible to USS1, but Op2 shouldn't have an OVN
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: None
+def test_read_ops_from_uss1(ids, vrp_session, vrp_session2):
+  if vrp_session is None:
+    return
+  time_now = datetime.datetime.utcnow()
+  end_time = time_now + datetime.timedelta(hours=1)
+  
+  resp = vrp_session.post('/operational_intent_references/query', 
+    json = {
+        'vertiport_reservation_of_interest': {
+                'time_start': make_time(time_now),
+                'time_end': make_time(end_time),
+                'vertiportid': 'ACDE070D-8C4C-4f0D-9d8A-162843c10333',
+                'reserved_zone': 0,
+        }
+  }, scope=SCOPE_VRP )
+  assert resp.status_code == 200, resp.content
+  
+  ops = {op['id']: op for op in resp.json().get('operational_intent_references', [])}
+  assert ids(OP1_TYPE) in ops
+  assert ids(OP2_TYPE) in ops
+  
+  ovn1 = ops[ids(OP1_TYPE)].get('ovn', '')
+  ovn2 = ops[ids(OP2_TYPE)].get('ovn', '')
+  assert ovn1 not in scd.NO_OVN_PHRASES
+  assert ovn2 in scd.NO_OVN_PHRASES
+
+
+# Op1 and Op2 should both be visible to USS2, but Op1 shouldn't have an OVN
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: None
+def test_read_ops_from_uss2(ids, vrp_session, vrp_session2):
+  if vrp_session2 is None:
+    return
+  time_now = datetime.datetime.utcnow()
+  end_time = time_now + datetime.timedelta(hours=1)
+  resp = vrp_session2.post('/operational_intent_references/query', 
+    json = {
+        'vertiport_reservation_of_interest': {
+                'time_start': make_time(time_now),
+                'time_end': make_time(end_time),
+                'vertiportid': 'ACDE070D-8C4C-4f0D-9d8A-162843c10333',
+                'reserved_zone': 0,
+        }
+  }, scope=SCOPE_VRP )
+  assert resp.status_code == 200, resp.content
+  
+  ops = {op['id']: op for op in resp.json().get('operational_intent_references', [])}
+  assert ids(OP1_TYPE) in ops
+  assert ids(OP2_TYPE) in ops
+  
+  ovn1 = ops[ids(OP1_TYPE)].get('ovn', '')
+  ovn2 = ops[ids(OP2_TYPE)].get('ovn', '')
+  assert ovn1 in scd.NO_OVN_PHRASES
+  assert ovn2 not in scd.NO_OVN_PHRASES
+
+
+# Try (unsuccessfully) to mutate Op1 with various bad keys
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: None
+def test_mutate_op1_bad_key(ids, vrp_session, vrp_session2):
+  resp = vrp_session.get('/operational_intent_references/{}'.format(ids(OP1_TYPE)), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  existing_op = resp.json().get('operational_intent_reference', None)
+  assert existing_op is not None, resp.content
+  
+  old_req = _make_op1_request()
+  req = {
+    'vertiport_reservation': old_req['vertiport_reservation'],
+    'old_version': existing_op['version'],
+    'state': 'Accepted',
+    'uss_base_url': URL_OP1,
+    'subscription_id': existing_op['subscription_id']
+  }
+  resp = vrp_session.put('/operational_intent_references/{}/{}'.format(ids(OP1_TYPE), op1_ovn), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 409, resp.content
+  missing_ops, _, _ = _parse_conflicts(resp.json())
+  assert ids(OP1_TYPE) in missing_ops
+  assert ids(OP2_TYPE) in missing_ops
+  
+  req['key'] = [op1_ovn]
+  resp = vrp_session.put('/operational_intent_references/{}/{}'.format(ids(OP1_TYPE), op1_ovn), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 409, resp.content
+  missing_ops, _, ovns = _parse_conflicts(resp.json())
+  assert ids(OP2_TYPE) in missing_ops
+  assert not(op2_ovn in ovns)
+  assert not(op1_ovn in ovns)
+  
+  req['key'] = [op2_ovn]
+  resp = vrp_session.put('/operational_intent_references/{}/{}'.format(ids(OP1_TYPE), op1_ovn), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 409, resp.content
+  missing_ops, _, ovns = _parse_conflicts(resp.json())
+  assert ids(OP1_TYPE) in missing_ops
+  assert not(op2_ovn in ovns)
+
+
+# Successfully mutate Op1
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Subscription Sub2 created by vrp_session2 user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: Operation Op1 mutated to second version
+def test_mutate_op1(ids, vrp_session, vrp_session2):
+  resp = vrp_session.get('/operational_intent_references/{}'.format(ids(OP1_TYPE)), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  existing_op = resp.json().get('operational_intent_reference', None)
+  assert existing_op is not None, resp.content
+  
+  global op1_ovn
+  
+  old_req = _make_op1_request()
+  req = {
+    'key': [op1_ovn, op2_ovn],
+    'vertiport_reservation': old_req['vertiport_reservation'],
+    'old_version': existing_op['version'],
+    'state': 'Accepted',
+    'uss_base_url': URL_OP1,
+    'subscription_id': existing_op['subscription_id']
+  }
+  resp = vrp_session.put('/operational_intent_references/{}/{}'.format(ids(OP1_TYPE), op1_ovn), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  
+  data = resp.json()
+  op = data['operational_intent_reference']
+  assert op['id'] == ids(OP1_TYPE)
+  assert op['uss_base_url'] == URL_OP1
+  assert op['version'] == 2
+  assert op['subscription_id'] == existing_op['subscription_id']
+  assert op['state'] == 'Accepted'
+  assert op.get('ovn', '')
+  
+  # USS1 should definitely be instructed to notify USS2's Subscription of the updated Operation
+  subscribers = _parse_subscribers(data.get('subscribers', []))
+  assert URL_SUB2 in subscribers, subscribers
+  
+  assert ids(SUB2_TYPE) in subscribers[URL_SUB2], subscribers[URL_SUB2]
+  assert subscribers[URL_SUB2][ids(SUB2_TYPE)] == 2
+  
+  op1_ovn = op['ovn']
+
+
+# Try (unsuccessfully) to delete the stand-alone Subscription that Op2 is relying on
+# Preconditions:
+#   * Subscription Sub2 created by vrp_session2 user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: None
+def test_delete_dependent_sub(ids, vrp_session, vrp_session2):
+  if vrp_session2 is None:
+    return
+  resp = vrp_session2.delete('/subscriptions/{}/{}'.format(ids(SUB2_TYPE), sub2_version), scope=SCOPE_VRP)
+  assert resp.status_code == 400, resp.content
+
+
+# Mutate the stand-alone Subscription
+# Preconditions:
+#   * Operation Op1 created by vrp_session user
+#   * Subscription Sub2 created by vrp_session2 user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: Subscription Sub2 mutated
+def test_mutate_sub2(ids, vrp_session, vrp_session2):
+  if vrp_session2 is None:
+    return
+  time_now = datetime.datetime.utcnow()
+  time_start = time_now - datetime.timedelta(minutes=1)
+  time_end = time_now + datetime.timedelta(minutes=61)
+  
+  # Create a good mutation request
+  req = _make_op2_request()
+  req['uss_base_url'] = URL_SUB2
+  del req['state']
+  req['notify_for_constraints'] = False
+  req['vertiport_reservation']['time_start'] = make_time(time_start)
+  req['vertiport_reservation']['time_end'] = make_time(time_end)
+  
+  req['notify_for_operational_intents'] = False
+  resp = vrp_session2.put('/subscriptions/{}'.format(ids(SUB2_TYPE)), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 400, resp.content
+  req['notify_for_operational_intents'] = True
+  
+  # Attempt mutation with start time that doesn't cover Op2
+  req['vertiport_reservation']['time_start'] = make_time(time_now + datetime.timedelta(minutes=5))
+  resp = vrp_session2.put('/subscriptions/{}/{}'.format(ids(SUB2_TYPE), sub2_version), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 400, resp.content
+  req['vertiport_reservation']['time_start'] = make_time(time_start)
+  
+  # Attempt mutation with end time that doesn't cover Op2
+  req['vertiport_reservation']['time_end'] = make_time(time_now)
+  resp = vrp_session2.put('/subscriptions/{}/{}'.format(ids(SUB2_TYPE), sub2_version), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 400, resp.content
+  req['vertiport_reservation']['time_end'] = make_time(time_end)
+  
+  # Attempt mutation without notifying for Operations
+  # Perform a valid mutation
+  resp = vrp_session2.put('/subscriptions/{}/{}'.format(ids(SUB2_TYPE), sub2_version), json=req, scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  
+  # The Subscription response should mention Op1 and Op2, but not include Op1's OVN
+  data = resp.json()
+  
+  ops = {op['id']: op for op in data['operational_intent_references']}
+  assert len(ops) >= 2
+  assert ops[ids(OP1_TYPE)].get('ovn', '') in scd.NO_OVN_PHRASES
+  assert ops[ids(OP2_TYPE)].get('ovn', '') not in scd.NO_OVN_PHRASES
+  
+  assert data['subscription']['notification_index'] == 2
+  
+  # Make sure the Subscription is still retrievable specifically
+  resp = vrp_session2.get('/subscriptions/{}'.format(ids(SUB2_TYPE)), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+
+
+# Delete Op1
+# Preconditions:
+#   * Subscription Sub2 created/mutated by vrp_session2 user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: Operation Op1 deleted
+def test_delete_op1(ids, vrp_session, vrp_session2):
+  resp = vrp_session.delete('/operational_intent_references/{}/{}'.format(ids(OP1_TYPE), op1_ovn), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  
+  data = resp.json()
+  op = data['operational_intent_reference']
+  
+  # USS1 should be instructed to notify USS2's Subscription of the deleted Operation
+  subscribers = _parse_subscribers(data.get('subscribers', []))
+  assert URL_SUB2 in subscribers, subscribers
+  assert ids(SUB2_TYPE) in subscribers[URL_SUB2], subscribers[URL_SUB2]
+  assert subscribers[URL_SUB2][ids(SUB2_TYPE)] == 3
+  
+  resp = vrp_session.get('/subscriptions/{}'.format(op['subscription_id']), scope=SCOPE_VRP)
+  print(resp.content)
+  assert resp.status_code == 404, resp.content
+
+
+# Delete Op2
+# Preconditions:
+#   * Operation Op1 deleted
+#   * Subscription Sub2 created/mutated by vrp_session2 user
+#   * Operation Op2 created by vrp_session2 user
+# Mutations: Operation Op2 deleted
+def test_delete_op2(ids, vrp_session, vrp_session2):
+  resp = vrp_session2.delete('/operational_intent_references/{}/{}'.format(ids(OP2_TYPE), op2_ovn), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+  
+  data = resp.json()
+  op = data['operational_intent_reference']
+  assert op['subscription_id'] == ids(SUB2_TYPE)
+  
+  # USS2 should be instructed to notify Sub2 of the deleted Operation
+  subscribers = _parse_subscribers(data.get('subscribers', []))
+  assert URL_SUB2 in subscribers, subscribers
+  assert ids(SUB2_TYPE) in subscribers[URL_SUB2], subscribers[URL_SUB2]
+  assert subscribers[URL_SUB2][ids(SUB2_TYPE)] == 4
+  
+  resp = vrp_session2.get('/subscriptions/{}'.format(ids(SUB2_TYPE)), scope=SCOPE_VRP)
+  assert resp.status_code == 200, resp.content
+
+
 # Delete Subscription used to serve Op2
 # Preconditions:
 #   * Operation Op1 deleted
